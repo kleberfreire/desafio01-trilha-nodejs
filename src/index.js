ef183db3de76c93278f8163a296ab778567821e3
Kleber Freire <ch4r4d4@gmail.com>
2021-12-18 12:04:32 -0300

    created post and get todos

--- src/index.js
+++ src/index.js
@@ -10,7 +10,23 @@ app.use(express.json());
 
 const users = [];
 
-function checksExistsUserAccount(request, response, next) {}
+function checksExistsUserAccount(request, response, next) {
+  const { username } = request.headers;
+  const user = users.find((user) => {
+    return user.username == username;
+  });
+  if (user) {
+    request.user = user;
+    request.idInUsers = indetifyIdUser(user);
+    return next();
+  }
+  return response.status(404).json({ message: "User does not exist" });
+}
+
+function indetifyIdUser(user) {
+  const idDataBaseUser = users.findIndex((userData) => userData.id === user.id);
+  return idDataBaseUser;
+}
 
 app.post("/users", (request, response) => {
   const { name, username } = request.body;
@@ -20,16 +36,25 @@ app.post("/users", (request, response) => {
     username: username,
     todos: [],
   });
-  console.log(users);
   return response.json({ message: "user created" });
 });
 
 app.get("/todos", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  return response.json(user.todos);
 });
 
 app.post("/todos", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user, idInUsers } = request;
+  const { title, done, deadline } = request.body;
+  users[idInUsers].todos.push({
+    id: uuidv4(),
+    title,
+    done,
+    deadline: new Date(deadline),
+    created_at: new Date(),
+  });
+  return response.json({ message: "todo created on success" });
 });
 
 app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
